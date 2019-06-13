97ca567e62aac62c7c71346695a8fc157d29c5af
Amy Tang <Amy@AmydeMacBook-Pro.local>
2019-06-12 23:06:08 -0700

    data updated

--- db-init/01-db-init.js
+++ db-init/01-db-init.js
@@ -44,327 +44,91 @@ db.users.insertMany([
 ]);
 
 db.courses.insertMany([
-  {
-    "subject": "CS",
-    "number": "493",
-    "title": "Cloud Application Development",
-    "term": "sp19",
-    "instructorId": "1",
-    "studentsId": ["3", "4"]
-  },
-  {
-    "subject": "CS",
-    "number": "480",
-    "title": "Translators",
-    "term": "sp19",
-    "instructorId": "1",
-    "studentsId": ["3", "4"]
-  },
-  {
+{
+    "_id": "5d01de295eec7adabe43630d",
     "subject": "CS",
     "number": "290",
     "title": "Web Development",
     "term": "sp19",
     "instructorId": "1",
-    "studentsId": ["3", "4", "5"]
-  },
-  {
+    "studentsId": [
+        "3",
+        "4",
+        "5"
+    ]
+},
+{
+    "_id": "5d01de295eec7adabe43630e",
     "subject": "MUS",
     "number": "103",
     "title": "Intron to Flute",
     "term": "sp19",
     "instructorId": "2",
-    "studentsId": ["3", "4", "5"]
-  },
-  {
+    "studentsId": [
+        "3",
+        "4",
+        "5"
+    ]
+},
+{
+    "_id": "5d01de295eec7adabe43630f",
     "subject": "MUS",
     "number": "303",
     "title": "Advanced Piano",
     "term": "sp19",
     "instructorId": "2",
-    "studentsId": ["3"]
-  },
-  {
+    "studentsId": [
+        "3"
+    ]
+},
+{
+    "_id": "5d01de295eec7adabe436310",
     "subject": "MUS",
     "number": "403",
     "title": "Advanced Piano II",
     "term": "sp19",
     "instructorId": "2",
-    "studentsId": ["3", "5"]
-  }
+    "studentsId": [
+        "3",
+        "5"
+    ]
+}
 ]);
 
 db.assignments.insertMany([
 {
-"courseId": "0",
-"title": "assignment1",
-"points": "90",
-"due": "2019-06-15T17:00:00-07:00"
+    "_id": "5d01de295eec7adabe436312",
+    "courseId": "5d01de295eec7adabe43630d",
+    "title": "assignment2",
+    "points": "100",
+    "due": "2019-06-30T17:00:00-07:00"
 },
 {
-"courseId": "0",
-"title": "assignment2",
-"points": "100",
-"due": "2019-06-30T17:00:00-07:00"
+    "_id": "5d01de295eec7adabe436313",
+    "courseId": "5d01de295eec7adabe43630d",
+    "title": "assignment1",
+    "points": "100",
+    "due": "2019-06-19T17:00:00-07:00"
 },
 {
-"courseId": "1",
-"title": "assignment1",
-"points": "100",
-"due": "2019-06-19T17:00:00-07:00"
+    "_id": "5d01de295eec7adabe436314",
+    "courseId": "5d01de295eec7adabe43630e",
+    "title": "assignment1",
+    "points": "100",
+    "due": "2019-06-19T17:00:00-07:00"
 },
 {
-"courseId": "2",
-"title": "assignment1",
-"points": "100",
-"due": "2019-06-19T17:00:00-07:00"
+    "_id": "5d01de295eec7adabe436315",
+    "courseId": "5d01de295eec7adabe43630f",
+    "title": "assignment1",
+    "points": "80",
+    "due": "2019-06-14T17:00:00-07:00"
 },
 {
-"courseId": "3",
-"title": "assignment1",
-"points": "80",
-"due": "2019-06-14T17:00:00-07:00"
-},
-{
-"courseId": "3",
-"title": "assignment2",
-"points": "50",
-"due": "2019-07-14T17:00:00-07:00"
+    "_id": "5d01de295eec7adabe436316",
+    "courseId": "5d01de295eec7adabe43630f",
+    "title": "assignment2",
+    "points": "50",
+    "due": "2019-07-14T17:00:00-07:00"
 }
 ]);
-
-db.businesses.insertMany([
-  {
-    "name": "Block 15",
-    "address": {
-      "street": "300 SW Jefferson Ave.",
-      "city": "Corvallis",
-      "state": "OR",
-      "zip": "97333"
-    },
-    "phone": "541-758-2077",
-    "category": "Restaurant",
-    "subcategory": "Brewpub",
-    "website": "http://block15.com"
-  },
-  {
-    "name": "Robnett's Hardware",
-    "address": {
-      "street": "400 SW 2nd St.",
-      "city": "Corvallis",
-      "state": "OR",
-      "zip": "97333"
-    },
-    "phone": "541-753-5531",
-    "category": "Shopping",
-    "subcategory": "Hardware"
-  },
-  {
-    "name": "Corvallis Brewing Supply",
-    "address": {
-      "street": "119 SW 4th St.",
-      "city": "Corvallis",
-      "state": "OR",
-      "zip": "97333"
-    },
-    "phone": "541-758-1674",
-    "category": "Shopping",
-    "subcategory": "Brewing Supply",
-    "website": "http://www.lickspigot.com"
-  },
-  {
-    "name": "First Alternative Co-op North Store",
-    "address": {
-      "street": "2855 NW Grant Ave.",
-      "city": "Corvallis",
-      "state": "OR",
-      "zip": "97330"
-    },
-    "phone": "541-452-3115",
-    "category": "Shopping",
-    "subcategory": "Groceries"
-  },
-  {
-    "name": "Local Boyz",
-    "address": {
-      "street": "1425 NW Monroe Ave.",
-      "city": "Corvallis",
-      "state": "OR",
-      "zip": "97330"
-    },
-    "phone": "541-754-5338",
-    "category": "Restaurant",
-    "subcategory": "Hawaiian"
-  },
-  {
-    "name": "Interzone",
-    "address": {
-      "street": "1563 NW Monroe Ave.",
-      "city": "Corvallis",
-      "state": "OR",
-      "zip": "97330"
-    },
-    "phone": "541-754-5965",
-    "category": "Restaurant",
-    "subcategory": "Coffee Shop"
-  },
-  {
-    "name": "Darkside Cinema",
-    "address": {
-      "street": "215 SW 4th St.",
-      "city": "Corvallis",
-      "state": "OR",
-      "zip": "97333"
-    },
-    "phone": "541-752-4161",
-    "category": "Entertainment",
-    "subcategory": "Movie Theater",
-    "website": "http://darksidecinema.com"
-  },
-  {
-    "name": "The Beanery Downtown",
-    "address": {
-      "street": "500 SW 2nd St.",
-      "city": "Corvallis",
-      "state": "OR",
-      "zip": "97333"
-    },
-    "phone": "541-753-7442",
-    "category": "Restaurant",
-    "subcategory": "Coffee Shop"
-  },
-  {
-    "name": "WinCo Foods",
-    "address": {
-      "street": "2335 NW Kings Blvd.",
-      "city": "Corvallis",
-      "state": "OR",
-      "zip": "97330"
-    },
-    "phone": "541-753-7002",
-    "category": "Shopping",
-    "subcategory": "Groceries"
-  },
-  {
-    "name": "The Book Bin",
-    "address": {
-      "street": "215 SW 4th St.",
-      "city": "Corvallis",
-      "state": "OR",
-      "zip": "97333"
-    },
-    "phone": "541-752-0040",
-    "category": "Shopping",
-    "subcategory": "Book Store"
-  },
-  {
-    "name": "Fred Meyer",
-    "address": {
-      "street": "777 NW Kings Blvd.",
-      "city": "Corvallis",
-      "state": "OR",
-      "zip": "97330"
-    },
-    "phone": "541-753-9116",
-    "category": "Shopping",
-    "subcategory": "Groceries"
-  },
-  {
-    "name": "Cyclotopia",
-    "address": {
-      "street": "435 SW 2nd St.",
-      "city": "Corvallis",
-      "state": "OR",
-      "zip": "97333"
-    },
-    "phone": "541-757-9694",
-    "category": "Shopping",
-    "subcategory": "Bicycle Shop"
-  },
-  {
-    "name": "Oregon Coffee & Tea",
-    "address": {
-      "street": "215 NW Monroe Ave.",
-      "city": "Corvallis",
-      "state": "OR",
-      "zip": "97333"
-    },
-    "phone": "541-752-2421",
-    "category": "Shopping",
-    "subcategory": "Tea House",
-    "website": "http://www.oregoncoffeeandtea.com"
-  },
-  {
-    "name": "Corvallis Cyclery",
-    "address": {
-      "street": "344 SW 2nd St.",
-      "city": "Corvallis",
-      "state": "OR",
-      "zip": "97333"
-    },
-    "phone": "541-752-5952",
-    "category": "Shopping",
-    "subcategory": "Bicycle Shop"
-  },
-  {
-    "name": "Spaeth Lumber",
-    "address": {
-      "street": "1585 NW 9th St.",
-      "city": "Corvallis",
-      "state": "OR",
-      "zip": "97330"
-    },
-    "phone": "541-752-1930",
-    "category": "Shopping",
-    "subcategory": "Hardware"
-  },
-  {
-    "name": "New Morning Bakery",
-    "address": {
-      "street": "219 SW 2nd St.",
-      "city": "Corvallis",
-      "state": "OR",
-      "zip": "97333"
-    },
-    "phone": "541-754-0181",
-    "category": "Restaurant",
-    "subcategory": "Bakery"
-  },
-  {
-    "name": "First Alternative Co-op South Store",
-    "address": {
-      "street": "1007 SE 3rd St.",
-      "city": "Corvallis",
-      "state": "OR",
-      "zip": "97333"
-    },
-    "phone": "541-753-3115",
-    "category": "Shopping",
-    "subcategory": "Groceries"
-  },
-  {
-    "name": "Block 15 Brewery & Tap Room",
-    "address": {
-      "street": "3415 SW Deschutes St.",
-      "city": "Corvallis",
-      "state": "OR",
-      "zip": "97333"
-    },
-    "phone": "541-752-2337",
-    "category": "Restaurant",
-    "subcategory": "Brewpub",
-    "website": "http://block15.com"
-  },
-  {
-    "name": "The Beanery Monroe",
-    "address": {
-      "street": "2541 NW Monroe Ave.",
-      "city": "Corvallis",
-      "state": "OR",
-      "zip": "97330"
-    },
-    "phone": "541-757-0828",
-    "category": "Restaurant",
-    "subcategory": "Coffee Shop"
-  }
-])
